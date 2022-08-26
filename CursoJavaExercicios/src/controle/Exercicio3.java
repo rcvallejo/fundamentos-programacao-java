8ce68a519c444c59f43d84a4e1e91278c1c3c761
rcvallejo <54600477+rcvallejo@users.noreply.github.com>
2022-08-25 23:23:17 -0400

    Update Exercicio3.java
    O código atual apresenta um erro: a média pode ser manipulada colocando números maiores que dez em uma nota, ainda que zere a outra. Exemplo: o programa aceitará a nota1 como: 20 e a nota2 como zero, porém, ainda assim, a média será 10 e o aluno estará "Aprovado"!
    A alteração do código permite que o programa identifique se as notas estão em um intervalo válido (de zero a dez) e adiciona a opção de imprimir na tela Notas inválidas, caso não estejam no intervalo válido de notas (de zero a dez).

--- CursoJavaExercicios/src/controle/Exercicio3.java
+++ CursoJavaExercicios/src/controle/Exercicio3.java
@@ -1,36 +1,38 @@
-package controle;
+package exercicioControle;
 
 import java.util.Scanner;
 
 public class Exercicio3 {
-	/**
-	 * 3. Criar um programa que receba duas notas parciais, calcular a média
-	 * final. Se a nota do aluno for maior ou igual a 7.0 imprime no console
-	 * "Aprovado", se a nota for menor que 7.0 e maior do que 4.0 imprime no console
-	 * "Recuperação", caso contrário imprime no console "Reprovado".
+	
+	/* 
+	 3. Criar um programa que receba duas notas parciais, calcular a média final. 
+	 Se a nota do aluno for maior ou igual a 7, imprime Aprovado
+	 Se a nota do aluno for maior ou igual a 7 e maior que 4, imprime Recuperação
+	 Caso contrário, Reprovado 
 	 */
-
+	
 	public static void main(String[] args) {
-
-		Scanner scanner = new Scanner(System.in);
-
-		System.out.println("Nota Parcial 1: ");
-		double nota1 = scanner.nextDouble();
-
-		System.out.println("Nota Parcial 2: ");
-		double nota2 = scanner.nextDouble();
-
+		
+		Scanner entrada = new Scanner(System.in);
+		System.out.print("Digite a primeira nota: ");
+		double nota1 = entrada.nextDouble();
+		System.out.print("Digite a segunda nota: ");
+		double nota2 = entrada.nextDouble();
+		
 		double media = (nota1 + nota2) / 2;
-
-		if (media >= 7) {
-			System.out.println("Sua média foi " + media + " e você está Aprovado.");
-		} else if (media >= 4) {
-			System.out.println("Sua média foi " + media + " e você está de Recuperação.");
-		} else {
-			System.out.println("Sua média foi " + media + " e você está Reprovado.");
-		}
-
-		scanner.close();
+		
+		if(nota1 >= 0 && nota1 <= 10 && nota2 >= 0 && nota2 <= 10) {
+			if(media >= 7) {
+				System.out.println("O aluno está aprovado!");
+			} else if(media >= 4 && media < 7) {
+				System.out.println("O aluno está em recuperação!");
+			} else
+				System.out.println("O aluno está reprovado!");
+			} else {
+				System.out.println("Notas inválidas!");
+			  }
+		
+		entrada.close();
+		
 	}
-
 }
