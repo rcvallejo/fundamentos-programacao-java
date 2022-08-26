574c44dcd06c3ea991754d836455748891bb1c9c
rcvallejo <54600477+rcvallejo@users.noreply.github.com>
2022-08-25 23:32:24 -0400

    Update Exercicio8.java
    A alteração leva em consideração os assuntos estudados até aqui e não o uso de vetores, os quais, teoricamente, ainda não podem "ser usados", já que não foram ensinados.

--- CursoJavaExercicios/src/controle/Exercicio8.java
+++ CursoJavaExercicios/src/controle/Exercicio8.java
@@ -1,25 +1,30 @@
-package controle;
+package exercicioControle;
 
 import java.util.Scanner;
 
 public class Exercicio8 {
+
 	/**
 	 * 8. Criar um programa que receba uma palavra e imprime no console letra por
 	 * letra.
 	 */
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		
+		Scanner entrada = new Scanner(System.in);
 
-		System.out.println("Digite a palavra: ");
-		String palavra = scanner.nextLine();
+		System.out.println("Digite uma palavra: ");
+		String palavra = entrada.next();
+		String suport = palavra;
+		int tamanho = palavra.length();
 
-		char letras[] = palavra.toCharArray();
-
-		for (int i = 0; i < letras.length; i++) {
-			System.out.println(letras[i]);
+		for(int i = 0; i < tamanho; i++) {
+			palavra = suport;
+			System.out.printf("%s", palavra.substring(0,1));
+			System.out.println();
+			suport = suport.substring(1);
 		}
-
-		scanner.close();
+		
+		entrada.close();
 	}
 }
